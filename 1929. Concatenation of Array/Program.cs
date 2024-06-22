89e233fadc21a23f2a3f3b71dacf7f661785ce13
pi1r <vandal18pioneer@gmail.com>
2024-06-22 20:32:06 +0300

    добавлена еще задача

---
+++ 1929. Concatenation of Array/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1929.Concatenation_of_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] nums = { 1, 2 };
+            int[] ans = new int [nums.Length * 2]; 
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                ans[i] = nums[i];    
+            }
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                ans[nums.Length + i] = nums[i]; 
+            }
+
+            for (int i = 0;i<ans.Length; i++)
+            {
+
+                Console.WriteLine(ans[i]);
+            }
+        }
+    }
+}
