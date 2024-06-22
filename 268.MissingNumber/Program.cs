2e11a471ca2b7cc2317399036ba1692740a2a52e
pi1r <vandal18pioneer@gmail.com>
2024-06-22 18:45:11 +0300

    Добавила первую задачу в репозиторий

---
+++ 268.MissingNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Programm
+{
+    static void Main(string[] args)
+    {
+        int[] num = {0,3,1};
+        int n = num.Length;
+        int waitSum = n * (n + 1) / 2;
+        int factSum = 0;
+        for (int i = 0; i < num.Length; i++)
+        {
+            factSum += num[i];
+        }
+
+        int result = waitSum - factSum;
+
+        Console.WriteLine(result);
+    }
+}
