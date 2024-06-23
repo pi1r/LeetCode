f7dbd9e2475f1d0dc6db0339adad1e202d193dd1
pi1r <vandal18pioneer@gmail.com>
2024-06-23 22:01:24 +0300

    _

---
+++ Minimum Add to Make Parentheses Valid/Minimum Add to Make Parentheses Valid/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    public int MinAddToMakeValid(string s)
+    {
+        int openNeeded = 0;
+        int closeNeeded = 0;
+
+        foreach (char c in s)
+        {
+            if (c == '(')
+            {
+                closeNeeded++;
+            }
+            else if (c == ')')
+            {
+                if (closeNeeded > 0)
+                {
+                    closeNeeded--;
+                }
+                else
+                {
+                    openNeeded++;
+                }
+            }
+        }
+
+        return openNeeded + closeNeeded;
+    }
+}
