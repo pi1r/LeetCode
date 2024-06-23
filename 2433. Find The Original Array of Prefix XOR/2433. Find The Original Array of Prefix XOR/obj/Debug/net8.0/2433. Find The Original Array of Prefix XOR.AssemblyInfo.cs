e26cbdedc45102f1693cae2980b222b2a1f40f6b
pi1r <vandal18pioneer@gmail.com>
2024-06-23 20:54:19 +0300

    Добавила задачу очередную

--- 2433. Find The Original Array of Prefix XOR/2433. Find The Original Array of Prefix XOR/obj/Debug/net8.0/2433. Find The Original Array of Prefix XOR.AssemblyInfo.cs
+++ 2433. Find The Original Array of Prefix XOR/2433. Find The Original Array of Prefix XOR/obj/Debug/net8.0/2433. Find The Original Array of Prefix XOR.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Minimum Add to Make Parentheses Valid")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("2433. Find The Original Array of Prefix XOR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Minimum Add to Make Parentheses Valid")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Minimum Add to Make Parentheses Valid")]
+[assembly: System.Reflection.AssemblyProductAttribute("2433. Find The Original Array of Prefix XOR")]
+[assembly: System.Reflection.AssemblyTitleAttribute("2433. Find The Original Array of Prefix XOR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
