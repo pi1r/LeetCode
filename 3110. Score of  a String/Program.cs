4c696f5e283ad023b76cac82a36417f88b3927f4
pi1r <vandal18pioneer@gmail.com>
2024-06-24 19:11:40 +0300

    easy

---
+++ 3110. Score of  a String/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+
+        string str = "hello";
+        int para = 0; 
+        int result = 0;
+
+        for (int i = 0; i < str.Length - 1; i++)
+        {
+            char c = str[i];
+            char next = str[i + 1]; 
+            para = Math.Abs((int)c - (int)next);
+            result = para + result;
+        }
+
+        Console.WriteLine(result);
+
+    }
+}
