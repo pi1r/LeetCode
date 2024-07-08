3dfea3f2fe6b96378c402939440851709e260b1e
pi1r <vandal18pioneer@gmail.com>
2024-07-08 23:04:37 +0300

    easy

---
+++ 771. Jewels and Stones/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _771.Jewels_and_Stones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string money = "z";
+            string myWallet = "Z";
+            int result = 0; 
+
+            for (int i= 0; i< money.Length; i++)
+            {
+                for (int j= 0; j<myWallet.Length; j++)
+                {
+                    if(money[i] == myWallet[j])
+                    {
+                        result++;
+                    }
+                }
+            }
+            
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
