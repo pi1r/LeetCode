3fc520fe7e91d42b4e1dc1b8b7a4c0aa3bf0be0a
pi1r <vandal18pioneer@gmail.com>
2024-07-08 22:11:44 +0300

    easy

---
+++ 2828. Check if a String Is an Acronym of Words/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace _2828.Check_if_a_String_Is_an_Acronym_of_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] array = { "pple", "banana", "combo",  "ппа" };
+            string str = "abc";
+            int c = 0;
+          
+            for (int i = 0;i < array.Length; i++)
+            {
+                if (array[i][c] == str[i])
+                {
+                    continue; 
+                }
+                else
+                {
+                    Console.WriteLine("нет");
+                    break;
+                }
+            }
+            Console.WriteLine("есть"); 
+
+        }
+    }
+}
